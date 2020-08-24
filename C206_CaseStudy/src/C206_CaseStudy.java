19562872ef0f5bd427489898c92c9d7fdb5710e4
Nix <64759633+NixWM@users.noreply.github.com>
2020-08-24 21:09:46 +0800

    Update C206_CaseStudy.java

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -9,6 +9,7 @@ public class C206_CaseStudy {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	ArrayList<Item> itemList = new ArrayList<Item>();
+	ArrayList<User> userList = new ArrayList<User>();
 
 	}
 }

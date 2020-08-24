55037f26c073f3de41d427b284d1722eb81c2947
19036308 <19036308@myrp.edu.sg>
2020-08-24 16:16:48 +0800

    Added addCategory, viewAllCategory and delCategory codes

---
+++ C206_CaseStudy/src/CategoryDB.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class CategoryDB {
+	
+	public static ArrayList<Category> categoryList = new ArrayList<Category>();
+
+	public static void addCategory(Category category) {
+
+		categoryList.add(category);
+		
+	}
+	
+	public static String viewAllCategory(ArrayList<Category> categoryList) {
+		String output = "";
+
+		for (int i = 0; i < categoryList.size(); i++) {
+
+			output += categoryList.get(i).getName()+"\n";
+		}
+		return output;
+	}
+	public static void delCategory(Category category) {
+
+		categoryList.remove(category);
+		
+	}
+	
+
+}

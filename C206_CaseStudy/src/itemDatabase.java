b351acb52acf44451e92201c238207237b1d8621
Nuurulhidayah <19040408@myrp.edu.sg>
2020-08-27 14:38:47 +0800

    new item(?)

--- C206_CaseStudy/src/itemDatabase.java
+++ C206_CaseStudy/src/itemDatabase.java
@@ -26,21 +26,7 @@ public class itemDatabase {
 	}
 
 	public static void delItem(String item1) {
-		Item a = null;
-		boolean b = false;
-		for(int c = 0; c < itemList.size(); c++) {
-			if(itemList.get(c).getName().equalsIgnoreCase(item1)) {
-				b = true;
-				a = itemList.get(c);
-			}
-		}
-		if(b == true) {
-			itemList.remove(a);
-			System.out.print("You have successfully deleted " + a);
-		}
-		else {
-			System.out.print("You have entered an invalid name");
-		}
+		itemList.removeAll(itemList);
 	}	
 	
 	

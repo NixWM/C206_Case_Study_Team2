90d713fc6ac7eef07542b8686e472599471e2038
BuildTools <unconfigured@null.spigotmc.org>
2020-08-27 20:09:31 +0800

    completed Deal.java, DealDB.java class

--- C206_CaseStudy/src/Deal.java
+++ C206_CaseStudy/src/Deal.java
@@ -1,12 +1,12 @@
 
 public class Deal {
 
-	public String dealID;
-	public String itemName;
-	public String sellerEmail;
-	public String buyerEmail;
-	public double dealPrice;
-	public String closeDate;
+	private String dealID;
+	private String itemName;
+	private String sellerEmail;
+	private String buyerEmail;
+	private double dealPrice;
+	private String closeDate;
 	
 	public Deal(String dealID, String itemName, String sellerEmail, String buyerEmail, double dealPrice,
 			String closeDate) {
@@ -22,54 +22,32 @@ public class Deal {
 		return dealID;
 	}
 
-	public void setDealID(String dealID) {
-		this.dealID = dealID;
-	}
-
 	public String getItemName() {
 		return itemName;
-	}
-
-	public void setItemName(String itemName) {
-		this.itemName = itemName;
 	}
 
 	public String getSellerEmail() {
 		return sellerEmail;
 	}
 
-	public void setSellerEmail(String sellerEmail) {
-		this.sellerEmail = sellerEmail;
-	}
-
 	public String getBuyerEmail() {
 		return buyerEmail;
-	}
-
-	public void setBuyerEmail(String buyerEmail) {
-		this.buyerEmail = buyerEmail;
 	}
 
 	public double getDealPrice() {
 		return dealPrice;
 	}
 
-	public void setDealPrice(double dealPrice) {
-		this.dealPrice = dealPrice;
-	}
-
 	public String getCloseDate() {
 		return closeDate;
 	}
 
-	public void setCloseDate(String closeDate) {
-		this.closeDate = closeDate;
-	}
-
 	@Override
 	public String toString() {
-		return "Deal [dealID=" + dealID + ", itemName=" + itemName + ", sellerEmail=" + sellerEmail + ", buyerEmail="
-				+ buyerEmail + ", dealPrice=" + dealPrice + ", closeDate=" + closeDate + "]";
+		String output = "";
+		output += String.format("%-10s, %-15s, %-25s, %-25s, $%-15.2f, %s\n", 
+				this.dealID, this.itemName, this.sellerEmail, this.buyerEmail, this.dealPrice, this.closeDate);
+		return output;
 	}
 	
 	
